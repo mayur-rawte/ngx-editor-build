d2dab5dd1fbf01084afef5f280f58ed6afdc2c89
Mayur <mayur@shipthis.co>
2018-04-19 18:51:46 +0530

    first commit

---
+++ app/ngx-editor/common/ngx-editor.defaults.d.ts
@@ -0,0 +1,31 @@
+/**
+ * toolbar default configuration
+ */
+export declare const ngxEditorConfig: {
+    editable: boolean;
+    spellcheck: boolean;
+    height: string;
+    minHeight: string;
+    width: string;
+    minWidth: string;
+    translate: string;
+    enableToolbar: boolean;
+    showToolbar: boolean;
+    placeholder: string;
+    imageEndPoint: string;
+    toolbar: string[][];
+    headers: {};
+    responseEndPoint: any;
+    appendImageEndPointToResponse: boolean;
+};
+/**
+ * codemirror configuaration
+ */
+export declare const codeMirrorConfig: {
+    lineNumbers: boolean;
+    gutter: boolean;
+    lineWrapping: boolean;
+    mode: string;
+    autofocus: boolean;
+    htmlMode: boolean;
+};
